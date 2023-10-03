7c1b36f3f024421f74f9634cc7a86cbdc01b69a4
Theophilus Ighalo <theophilusighalo@gmail.com>
2023-10-03 10:26:14 +0100

    web pack config

--- client/next.config.js
+++ client/next.config.js
@@ -4,15 +4,15 @@ const nextConfig = {
   images: {
     domains: ['thrangra.sirv.com', 'i.pravatar.cc', 'utfs.io'],
   },
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.(graphql|gql)/,
-      exclude: /node_modules/,
-      loader: 'graphql-tag/loader',
-    });
+  // webpack: (config, options) => {
+  //   config.module.rules.push({
+  //     test: /\.(graphql|gql)/,
+  //     exclude: /node_modules/,
+  //     loader: 'graphql-tag/loader',
+  //   });
 
-    return config;
-  },
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
